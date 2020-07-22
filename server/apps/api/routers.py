a947e6e5e3806e040aea748c6df0cd8de149e39c
ray <ottotruepro@gmail.com>
2020-07-22 23:23:00 +0700

    Добавил view в routers.py

--- server/apps/api/routers.py
+++ server/apps/api/routers.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
 
 from apps.test.viewsets import TestViewSet
+from apps.users.viewsets import UsersViewSet
 
 
 router = routers.DefaultRouter()
 router.register('test', TestViewSet, basename='test')
+router.register('users', UsersViewSet, basename='users')
