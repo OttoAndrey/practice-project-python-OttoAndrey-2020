283e92cba693ac8b00c14185e84cb91160c81273
ray <ottotruepro@gmail.com>
2020-07-25 21:10:02 +0700

    Добавил модель catering в routers.py

--- server/apps/api/routers.py
+++ server/apps/api/routers.py
@@ -2,8 +2,10 @@ from rest_framework import routers
 
 from apps.test.viewsets import TestViewSet
 from apps.users.viewsets import UsersViewSet
+from apps.catering.viewsets import CateringViewSet
 
 
 router = routers.DefaultRouter()
 router.register('test', TestViewSet, basename='test')
 router.register('users', UsersViewSet, basename='users')
+router.register('catering', CateringViewSet, basename='catering')
