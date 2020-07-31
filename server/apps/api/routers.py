f60912e85ff6cb4962b5d686ffa75f20cc564758
ray <ottotruepro@gmail.com>
2020-07-31 21:56:59 +0700

    Добавил приложения в routers.py

--- server/apps/api/routers.py
+++ server/apps/api/routers.py
@@ -1,11 +1,14 @@
 from rest_framework import routers
 
+from apps.catering.viewsets import CateringViewSet
+from apps.dish.viewsets.dish import DishViewSet
+from apps.ingredient.viewsets.ingredient import IngredientViewSet
 from apps.test.viewsets import TestViewSet
 from apps.users.viewsets import UsersViewSet
-from apps.catering.viewsets import CateringViewSet
-
 
 router = routers.DefaultRouter()
 router.register('test', TestViewSet, basename='test')
 router.register('users', UsersViewSet, basename='users')
 router.register('catering', CateringViewSet, basename='catering')
+router.register('dish', DishViewSet, basename='dish')
+router.register('ingredient', IngredientViewSet, basename='ingredient')
