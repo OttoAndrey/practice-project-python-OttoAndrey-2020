9de806d3d6ea5b01c0fb485bd195280b79e6ed7c
ray <ottotruepro@gmail.com>
2020-08-03 11:24:24 +0700

    Добавил права доступа во вью заведения

--- server/apps/catering/viewsets/catering.py
+++ server/apps/catering/viewsets/catering.py
@@ -2,8 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from apps.catering.models import Catering
 from apps.catering.serializers import CateringSerializer
+from apps.main.permissions import IsOwnerOrReadOnlyCateringPermission
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class CateringViewSet(ModelViewSet):
     serializer_class = CateringSerializer
     queryset = Catering.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnlyCateringPermission,)
