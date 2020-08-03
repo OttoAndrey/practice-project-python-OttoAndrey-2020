740e36bebe7927fbe4f96b8111a6c72acc659e60
ray <ottotruepro@gmail.com>
2020-08-03 11:27:10 +0700

    Добавил права доступа во вьюсет блюда

--- server/apps/dish/viewsets/dish.py
+++ server/apps/dish/viewsets/dish.py
@@ -2,8 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from apps.dish.models import Dish
 from apps.dish.serializers import DishSerializer
+from apps.main.permissions.dish import IsOwnerOrReadOnlyDishPermission
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class DishViewSet(ModelViewSet):
     serializer_class = DishSerializer
     queryset = Dish.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnlyDishPermission,)
