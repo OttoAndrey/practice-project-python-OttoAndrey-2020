07b9fae1d1cef162a497d9a6921644e051b01284
ray <ottotruepro@gmail.com>
2020-08-03 11:27:49 +0700

    Добавил права доступа во вьюсет ингредиентов

--- server/apps/ingredient/viewsets/ingredient.py
+++ server/apps/ingredient/viewsets/ingredient.py
@@ -1,12 +1,11 @@
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
 from apps.ingredient.models import Ingredient
 from apps.ingredient.serializers import IngredientSerializer
 
 
-class IngredientViewSet(mixins.ListModelMixin,
-                        mixins.RetrieveModelMixin,
-                        GenericViewSet):
+class IngredientViewSet(ModelViewSet):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
