c251c6adcb2d145f9e6aceeee650fd597014b3c5
ray <ottotruepro@gmail.com>
2020-08-04 13:49:26 +0700

    Комментарии для блюда

--- server/apps/dish/viewsets/dish.py
+++ server/apps/dish/viewsets/dish.py
@@ -7,6 +7,28 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class DishViewSet(ModelViewSet):
+    """
+    list:
+    Список всех блюд.
+
+    create:
+    Создаёт объект блюда. Поля sum_of_calories и owner рассчитываются автоматически.
+    Создавать блюда могут только авторизированные пользователи.
+
+    retrieve:
+    Отображает только одно созданное блюдо.
+
+    update:
+    Обновляет объект блюда. Обновить могут только владельцы блюда.
+
+    partial_update:
+    Обновляет объект блюда частично. Обновить могут только владельцы блюда.
+
+    destroy:
+    Удаляет объект блюда.
+    Удалить могут только владельцы блюда.
+    """
+
     serializer_class = DishSerializer
     queryset = Dish.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,
