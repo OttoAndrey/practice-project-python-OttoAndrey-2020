13350c589dad435042e807cb94a86fc7ee3681ad
ray <ottotruepro@gmail.com>
2020-08-04 13:49:06 +0700

    Комментарии для заведения

--- server/apps/catering/viewsets/catering.py
+++ server/apps/catering/viewsets/catering.py
@@ -7,6 +7,28 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class CateringViewSet(ModelViewSet):
+    """
+    list:
+    Список всех заведений.
+
+    create:
+    Создаёт заведение.
+    Поля latitude, longitude, owner только на чтение и рассчитываются автоматически при создании объекта.
+    Создавать заведения могут только авторизированные пользователи.
+
+    retrieve:
+    Один объект заведения.
+
+    update:
+    Обновление объекта. Обновлять заведения могут только владельцы этого заведения.
+
+    partial_update:
+    Обновление некоторых полей объекта. Обновлять заведения могут только владельцы этого заведения.
+
+    destroy:
+    Удаление объекта. Удалить заведение может только владелец этого заведения.
+    """
+
     serializer_class = CateringSerializer
     queryset = Catering.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,
