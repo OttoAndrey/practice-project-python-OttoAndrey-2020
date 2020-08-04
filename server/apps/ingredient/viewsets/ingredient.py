1288a7ec449ccedeb511035e342b97b9132ae526
ray <ottotruepro@gmail.com>
2020-08-04 13:50:28 +0700

    Комментарии для ингредиентов. Изменил наследование - убрал методы удаления и изменения

--- server/apps/ingredient/viewsets/ingredient.py
+++ server/apps/ingredient/viewsets/ingredient.py
@@ -1,11 +1,26 @@
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 
 from apps.ingredient.models import Ingredient
 from apps.ingredient.serializers import IngredientSerializer
 
 
-class IngredientViewSet(ModelViewSet):
+class IngredientViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin,
+                        GenericViewSet):
+    """
+    list:
+    Список всех ингредиентов.
+
+    create:
+    Создаёт объект ингредиента. Создавать могут только авторизированные пользователи.
+
+    retrieve:
+    Отображает только один ингредиент.
+    """
+
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
